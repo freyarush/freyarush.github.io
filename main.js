fe1e632cab1af04d1af213907880132a46a7263d
freyarush <100817992+freyarush@users.noreply.github.com>
2022-05-05 16:04:39 +1000

    created list page

--- main.js
+++ main.js
@@ -3,6 +3,22 @@ function buttonClick(buttonElement) {
     console.log("Hey you clicked " + buttonElement.textContent);
 }
 
+const getData = (pageurl) => {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.onload = function();
+            if (this.readyState == 4 && this.status == 200){
+                resolve(this.reponseText);
+            } else {
+                reject(this.responseText);
+            }
+            request.open("get", pageUrl, true);
+            request.send();
+    })
+}
+
+
+
 window.addEventListener("DOMContentLoaded", function()m{}
     var buttonsList = document.getElementsByClassName("button");
 
