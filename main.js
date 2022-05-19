9c23c275beee32cea561d263d0739763c8100faa
freyarush <100817992+freyarush@users.noreply.github.com>
2022-05-19 14:25:53 +1000

    dropdown menu

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
-function buttonClick(buttonElement) {
-    alert("Hey you clicked " + buttonElement.textContent);
-    console.log("Hey you clicked " + buttonElement.textContent);
-}
 
-const getData = (pageurl) => {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        request.onload = function();
-            if (this.readyState == 4 && this.status == 200){
-                resolve(this.reponseText);
-            } else {
-                reject(this.responseText);
-            }
-            request.open("get", pageUrl, true);
-            request.send();
-    })
-}
+  
 
+  //function toggleResponsive() {
+    //var navbar = document.getElementByID("myTopnav");
+  // navbar.classlist.toggle("responsive)")
+ // }
 
-
-window.addEventListener("DOMContentLoaded", function()m{}
-    var buttonsList = document.getElementsByClassName("button");
-
-    for( const button of buttonsList){
-        button.addEventListener("click", function(event) {}
-            buttonClick(event.target);
-        })
-    }
-});
