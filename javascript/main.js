b4408c707d1aea9c2ba89158e8170c3faef2b241
freyarush <100817992+freyarush@users.noreply.github.com>
2022-06-03 12:07:26 +1000

    java clicks working

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-//document.body.innerHTML = "<p>it is linked</p>"
+//document.body.innerHTML = "<p>it is linked</p>";
 
 
   //function toggleResponsive() {
@@ -10,18 +10,47 @@
 var hamburger = document.getElementsByClassName("hamburger");
 var mobileMenuItem = document.getElementsByClassName("mobile-menu-item");*/
 
+function searchBar(){
+  alert("it works");
+  /*if (document.getElementByClassName("search-bar").style.display == "none";){
+    alert("it works");
+  } else{
+    alert("it works!!!");
+
+  }*/
+
+  //elt.style.cssText = "display: flex;";
+  /*let x = document.getElementsByClassName("search-bar");
+
+  if (x.main.display === "none") {
+    x.main.display = "flex";
+  } else {
+    x.main.display = "none";
+  }*/
+
+}
+
+/*var x = document.getElementID("search-bar");
+if (x.style.display === "none") {
+  x.style.display = "flex";
+} else {
+  x.style.display = "none";
+}
+
+search-icon.addEventListener("click", searchBar);*/
+
 function triggerVisibility() {
-  var menu = document.getElementsByClassName("mobile-menu");
+  alert("it works");
+  /*var menu = document.getElementsByClassName("mobile-menu");
 
   if (menu.style.display == "none"){ 
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
-  }
+  }*/
 
 }
 
-hamburger.addEventListener("click", triggerVisibility);
 
 /*if (document.getElementsByClassName("hamburger").clicked == true){
   mobileMenuItem.style.display = "block";
