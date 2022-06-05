1a614a9787c926c76e7bad04fa46fa8e0aab05f4
freyarush <100817992+freyarush@users.noreply.github.com>
2022-06-05 16:44:31 +1000

    made hamburger expandable

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,3 @@
-//document.body.innerHTML = "<p>it is linked</p>";
-
-
-  //function toggleResponsive() {
-    //var navbar = document.getElementByID("topnav");
-  // navbar.classlist.toggle("responsive)")
- // }
-
-/*var mobileMenu = document.getElementsByClassName("mobile-menu");
-var hamburger = document.getElementsByClassName("hamburger");
-var mobileMenuItem = document.getElementsByClassName("mobile-menu-item");*/
 
 function searchBar(){
   var searchSection = document.getElementById("searchBar");
@@ -20,55 +9,12 @@ function triggerHamburger() {
   hamburgerMenu.classList.toggle("responsive");
 }
 
+function triggerExpandHamburger() {
+  var hamburgerExpand = document.getElementById("hamburgerExpand");
+  hamburgerExpand.classList.toggle("responsive-block");
+}
+
 function triggerSummary() {
   var orderSection = document.getElementById("orderSummary");
   orderSection.classList.toggle("responsive-block");
 }
-
-
-  
-  /*var menu = document.getElementsByClassName("mobile-menu");
-
-  if (menu.style.display == "none"){ 
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }*/
-
-
-  //document.getElementByID("search-bar").style.display = "flex";
-
-  /*if (document.getElementByClassName("search-bar").style.display == "none";){
-    alert("it works");
-  } else{
-    alert("it works!!!");
-
-  }*/
-
-  //elt.style.cssText = "display: flex;";
-  /*let x = document.getElementsByClassName("search-bar");
-
-  if (x.main.display === "none") {
-    x.main.display = "flex";
-  } else {
-    x.main.display = "none";
-  }*/
-
-
-/*var x = document.getElementID("search-bar");
-if (x.style.display === "none") {
-  x.style.display = "flex";
-} else {
-  x.style.display = "none";
-}
-
-search-icon.addEventListener("click", searchBar);*/
-
-
-/*if (document.getElementsByClassName("hamburger").clicked == true){
-  mobileMenuItem.style.display = "block";
-  alert("clicked");
-} else {
-  mobileMenuItem.style.display = "none";
-}*/
-
