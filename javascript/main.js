9db562e1027ea226a1ce08ff92cb9f1c274d3dd6
freyarush <100817992+freyarush@users.noreply.github.com>
2022-06-07 15:30:37 +1000

    added comments and spaced code

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,23 @@
 
+//show and hide search bar
 function searchBar(){
   var searchSection = document.getElementById("searchBar");
   searchSection.classList.toggle("responsive");
 }
 
+//show and hide hamburger menu
 function triggerHamburger() {
   var hamburgerMenu = document.getElementById("hamburger");
   hamburgerMenu.classList.toggle("responsive");
 }
 
+//show and hide hamburger expanded menu
 function triggerExpandHamburger() {
   var hamburgerExpand = document.getElementById("hamburgerExpand");
   hamburgerExpand.classList.toggle("responsive-block");
 }
 
+//show and hide mobile checkout order summary
 function triggerSummary() {
   var orderSection = document.getElementById("orderSummary");
   orderSection.classList.toggle("responsive-block");
